88a9ac5d5c88a5a7c76ba4761d458243885a1486
Mohammad Ali Asif <100618338+Rappidz456@users.noreply.github.com>
2025-01-20 18:34:55 +0500

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  exportPathMap(defaultMap, ctx) {
+    return {
+      "/": { page: "/" },
+      "/home": { page: "/home" },
+      "/register": { page: "/register" },
+    };
+  },
 };
 
 export default nextConfig;
