5a5837e2895f56d1183ab5af154f3e4d9fd882fb
Mohammad Ali Asif <100618338+Rappidz456@users.noreply.github.com>
2025-01-21 18:07:28 +0500

    changes

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,19 +1,24 @@
-import React from "react";
+"use client";
+import React, { JSX } from "react";
 import "./styles.css";
 import Image from "next/image";
-import LogoItem from "../../../public/LogoItem.png"
+import LogoItem from "../../../public/LogoItem.png";
+import { useRouter } from "next/navigation";
 
-const page = () => {
+const page: React.FC = (): JSX.Element => {
+  const router = useRouter();
+  React.useEffect(() => {
+    router.push("/home");
+    return () => window.location.reload();
+  }, [router]);
   return (
     <div className="contaner">
       <div className="logo">
-        <Image src={LogoItem} alt="Logo" width={400} height={400}/>
+        <Image src={LogoItem} alt="Logo" width={400} height={400} />
       </div>
       <div className="contents">
         <h1 className="heading">Discover Delicious Vegan Snacks</h1>
-        <p className="subheading">
-          Nourish Your Body
-        </p>
+        <p className="subheading">Nourish Your Body</p>
       </div>
     </div>
   );
